e4f5dd77bb5ce28d620e01af2286f77ddb930349
Arina Ulanova <ul.arina09@gmail.com>
2021-08-09 23:43:48 +0300

    terraform basic hw

---
+++ homeworks/virtual/terraform_basic.md
@@ -0,0 +1,19 @@
+# Домашнее задание к занятию "7.3. Основы и принцип работы Терраформ"
+
+https://github.com/netology-code/virt-homeworks/tree/master/07-terraform-03-basic
+
+## Задача 1. Создадим бэкэнд в S3
+
+
+## Задача 2. Инициализируем проект и создаем воркспейсы
+![image info](../images/virtual/terraform_basic/task2.png)
+
+terraform plan:
+![image info](../images/virtual/terraform_basic/task2_1.png)
+
+После terraform apply (после задачи 1):
+![image info](../images/virtual/terraform_basic/task2_2.png)
+
+
+
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,30 +4,62 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
 
+  owners = ["amazon"]
+
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    name = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name = "owner-alias"
+    values = ["amazon"]
   }
-
-  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+
+locals {
+  web_instance_type_map = {
+  stage = "t3.micro"
+  prod = "t3.large"
+  }
+}
+
+
+locals {
+  web_instance_count_map = {
+  stage = 1
+  prod = 2
+  }
+}
+
+
+resource "aws_instance" "count_web" {
+  ami = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+  count = local.web_instance_count_map[terraform.workspace]
+}
 
-  tags = {
-    Name = "Netology"
+locals {
+  instances = {
+  "t3.micro" = data.aws_ami.amazon_linux.id
+  "t3.large" = data.aws_ami.amazon_linux.id
   }
 }
+
+resource "aws_instance" "for_each_web" {
+  for_each = local.instances
+  ami = each.value
+  instance_type = each.key
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 
 data "aws_caller_identity" "current" {}
 
